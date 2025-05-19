23c1c7c10bec5bf1bd917cf79a970fb98fc546f3
Ana Razvan-Adrian <anarazvan7@yahoo.com>
2025-05-19 22:52:31 +0300

    Ai Enemy

---
+++ Assets/Scripts/Enemy_Movement.cs
@@ -0,0 +1,48 @@
+ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Movement : MonoBehaviour
+{
+    private bool isChasing;
+    public float speed;
+    private Rigidbody2D rb;
+    private Transform player;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+    void Update()
+    {
+        if (isChasing == true)
+        {
+            Vector2 direction = (player.position - transform.position).normalized;
+            rb.velocity = direction * speed;
+        }
+    }
+
+         private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            if (player == null)
+            {
+                player = collision.transform;
+            }  
+
+            isChasing = true;
+        }  
+    }
+     
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            isChasing = false;
+            rb.velocity = Vector2.zero;
+        }
+    }
+}
